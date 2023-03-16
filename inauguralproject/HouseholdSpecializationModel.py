173c808e41ad2541e830bb4eb715d09361473c54
vtk426 <ketsinson@gmail.com>
2023-03-16 20:09:36 +0100

    Exercise 1

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -1,4 +1,3 @@
-
 from types import SimpleNamespace
 
 import numpy as np
@@ -24,7 +23,8 @@ class HouseholdSpecializationModelClass:
 
         # c. household production
         par.alpha = 0.5
-        par.sigma = 1.0
+        par.sigma = 1
+
 
         # d. wages
         par.wM = 1.0
@@ -54,7 +54,12 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma == 0:
+            H = np.minimum(HM,HF)
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(((par.sigma-1)/par.sigma))
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
